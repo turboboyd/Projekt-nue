6de0159c11f41a3d6b37b240e53a2bad188efa73
Denis Daniv <denisdaniv1@gmail.com>
2025-12-18 20:41:57 +0100

    fix

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,24 +1,26 @@
-import { classNames } from 'shared/lib';
-import { AppLink, AppLinkTheme } from 'shared/ui';
-import { useTranslation } from 'react-i18next';
-import styles from './Navbar.module.scss';
+import { classNames } from "shared/lib";
+import { AppLink, AppLinkTheme } from "shared/ui";
+import { useTranslation } from "react-i18next";
+import styles from "./Navbar.module.scss";
 
 interface NavbarProps {
     className?: string;
 }
-const { t } = useTranslation();
 
-const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(styles.Navbar, {}, [className])}>
-        <div className={styles.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} className={styles.linkMain} to="/">
-                {t('Home')}
-            </AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} className={styles.link} to="/about">
-                {t('About')}
-            </AppLink>
+const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(styles.Navbar, {}, [className])}>
+            <div className={styles.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} className={styles.linkMain} to="/">
+                    {t("Home")}
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} className={styles.link} to="/about">
+                    {t("About")}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Navbar;
