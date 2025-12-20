6cac1add9a6df332ed84c5f6ff4a0d3e3d0ff144
Denis Daniv <denisdaniv1@gmail.com>
2025-12-20 13:01:55 +0100

    Fix MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib';
+import styles from './MainPage.module.scss';
 
-const MainPage = () => <div>MainPage</div>;
+interface MainPageProps {
+    className?: string;
+}
 
-export default MainPage;
+export const MainPage: React.FC<MainPageProps> = ({ className }) => {
+    const { t } = useTranslation('MainPage');
+    return <div className={classNames(styles.MainPage, {}, [className])}>{t('Main Page')}</div>;
+};
