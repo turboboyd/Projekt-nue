54de8d382d668cf979114b7c0c5fa225b126634f
Denis Daniv <denisdaniv1@gmail.com>
2025-12-20 13:25:08 +0100

    fix lint

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
-import { classNames } from "shared/lib";
-import styles from "./MainPage.module.scss";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib';
+import styles from './MainPage.module.scss';
 
 interface MainPageProps {
     className?: string;
 }
 
- const MainPage: React.FC<MainPageProps> = ({ className }) => {
-    const { t } = useTranslation("MainPage");
-    return <div className={classNames(styles.MainPage, {}, [className])}>{t("Main Page")}</div>;
+const MainPage: React.FC<MainPageProps> = ({ className }) => {
+    const { t } = useTranslation('MainPage');
+    return <div className={classNames(styles.MainPage, {}, [className])}>{t('Main Page')}</div>;
 };
-
 
 export default MainPage;
